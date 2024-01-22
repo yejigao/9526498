14b328bde6be228a1aa663b4cd2a47e6882ce5dc
yejigao <yejigao@126.com>
2024-01-22 09:54:23 +0000

    Update imaotai.py

--- imaotai.py
+++ imaotai.py
@@ -17,7 +17,7 @@ import json #line:8
 #原创 微信公众号@爱上羊毛侠 
 
 # 青龙面板加入环境变量MTTokenD
-# MTTokenD是茅台预约参数，多个请换行，格式'省份,城市,经度,维度,MT-Device-ID的值,MT-Token,MT-Token-Wap的值(抓包小茅运)'
+# MTTokenD是茅台预约参数，多个请换行，格式'省份,城市,经度,维度,MT-Device-ID的值,MT-Token的值,MT-Token-Wap的值(抓包小茅运)'
 p_c_map ={}#line:1
 mt_r ='clips_OlU6TmFRag5rCXwbNAQ/Tz1SKlN8THcecBp/'#line:2
 res_map ={'10941':'贵州茅台酒（甲辰龙年）','10942':'贵州茅台酒（甲辰龙年）x2'}#line:7#,'2478':'贵州茅台酒（珍品）'
