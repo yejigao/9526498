c1ec0aef5753be83aacec16a96a6e31af18ccbbb
yejigao <yejigao@126.com>
2024-02-18 12:26:35 +0000

    替换 甬派.py

--- 甬派.py
+++ 甬派.py
@@ -1,9 +1,258 @@
-# -*- coding: utf-8 -*-
-"""
-new Env('甬派');
-先运行脚本，有问题到群里问 http://t.me/xiaoymgroup
-通知模块请复制青龙的notify.py到脚本同级目录
-"""
-import zlib
-import base64
-exec(zlib.decompress(base64.b64decode('eJzNWntvE9kV/z+fYuptGBuc8YzfDmshWqnd1S7rVFCtkEmtiX1tD7FnpjPjJCaKBK3YhtdCyxb2kS5FYrtVhRaoKK8Q+l12Yyf5i6/Q+57HHYegrtQ6UeJM7jnndx73nHPvsdG3LceTWroHPKMPpHd6hglmS1MGeX7WtUz6rMyeWS59UmFPHN1sWX36VFP5Y8ZO09gjr+sAvWWYHfaf7FTbgaS/HYABkOiiX+E/6IIcJ/XhaXlfBKR0PYZIK0y1QFvqAK/RdztSMjVLn5enJPiq1VT0hb5V9L4mVVcTv3aBM3O8A0wvMZs4YZ0zej09U1BUKfmxAdVadqWPTkmaqqhHJfigmD8qrRTzKem4bffAx2DhA8PLFHIlJVeUkh+8d+rEh2mpZywC6ZeguWilpJ93oXogo2mQI/qSTupt3TEoSWKN4MuqHB+Gh36jn1KVK6hApaSk3PU8253NZM4a3UFPX1CaVj+zYpzrGroBf+vWsJ+ZyTj6cqavG2ZmCTgKcqGc7kKzA8eVqqINUngJNBbFomEsDvAGjhkDia7KTjXII/ID/5SqvuXpMuKQNnYed0eWuaPGUBD2UpXHoeK/M61lSKq4nkPZyNOnZ6b7M9Mtafq92ekTs9MnZSauEsYe5U9W5VQMqmfprYZpeUZ76EPL5TCHTs9a0HuSC8wWI8rjf3jOUGJLC/gJeuEYpqxoZAZJi3yh7RgmdGNidPmvu1tbe+e/3LnzzXjj2ujy3fH6jdHlO69fXkhQXXIlTkX1OeXwbZEjFgQrTWB7HFAlKkcW5YzuPdp9/E1698HT0b1ro/Un28/Ob29u7f3lT3uvviAaKPZwtP5wdOPqzotvx7f+sL35ZHTvObNwXo2i+oXecxmsvDbV7OmuK52eOz43x3DliemQzRsNwzS8RkNKYudg/+DYgc5JEy+pNKRQPKXZEr5YYo7K+9YXWSkucF0DBnVg/7BHLDTzxf0YtMCS0QRGC24YAYLi2j0DWfcdOVVXpXnKr7QfP89oLsIN/CZuGudWPoh6Ct/Wq7Kfx+RZ2VpEiSKTVyqKJhttlIqVvu41u1LSkX9T12fOqTOV+cM/ldP76Z4CyLXy3IenLbMzpxtzjnUWNL0MzIaaoklJY65rmeCoZNROSjDBlZTKUelkU++BTFZR1ZSclnXbhjkIYYWgCBl8Sowhz062U1pmIGJXcYRpmkAL+0YDzONgBds+El+UeN9IMMGy+34LmrhOXVPY19Fu04LGrnLe+/pxAH2GYuwjaEZGUNmPAOXWqiyTpUWV7yuUdzEzs23BvUVIeJr3d00x63NXA2mdZG9eXYY2dJzSNE1cYOCbzDJYmDEXms1MH/QXgMN+IXkMTS7AmsY4y77I9lFIfhDjyibCidSlYj5oGJUzJHUcL1VQSncRr6h8Wj1hUdHlFGNYCNkigo65RpRFeaEF77dYXiwGY4j6i1ZMqH17H8sO4d6ydWMG8cvotpHBb/rDRg8sgd4xIqa6KkCkCNeY+Ush8zMMGMJBzC+Ajpq/vJ+1aNSLkkOGp3/gxdxwfrzDPDXZETSAS379mUxC0LxbUDmVFqLiRqKdAWaAvPTD+Rs7X2yOtv78w/k/1ncf/210/WmM4XE6WZuXyFJSuNM7nz4cb1za+fbBaP2TGBoMifmqlA3BwVmWIc39fyB9/fLq6N6d3a378Hvnq8+3X3zCsOdDAGmjIWKk7i0VpsK6CM5CKe1IVWRwRD5jMpnFEBOxHSr5wR8yZjki/L825O6nT0fXb5Fl2/++O77wgPRTDGjlrYxTVn8E45S1OOMEu7JyNlQnUEEzSJpklYXK5WWinI8kE76hUa7FSpHenPb1hzXUyTCtfJcvd40ekHrAjBPGK2sqsFPLYVe3LUeieZclJojAMNGRswMlF3zIJXHjhOonLhMHScQIFk7ES9kMft8zXO9Ys6ubJuiRVBwBtHaoOXAc2HnN6R1Q1Q4hq7ie3rfx2rD5WA4ol+M2uu/pGi4cMVaL5u2ojpG8Xa4IcnDSjIqiublptWBq/kmVO6SiCvSB0JbJhhjferh3e5NuBSq4IuZc9GpapmeYfOdWsjFuU/0TJ257qiLcOikp83UZMURNL+3OKmICxUHEnMb2GAoiURJTOh+LHRlO4EMNR4JZZuFYKcRyIG6euBMUGJPo3JgUxdSZhHlm32JMAE3kXEVBLCVd1mpN2oqMeYmnDHRhg3MGJuQHfmw1dkCr0e6NnNlqNW5SvIzv0UollFZ4n1Cjufgt9iZ+0wKebvSOEfBwr0UFrx0KdE9h5JHuCeavSGOsMgraPkUUF7rXqDqRbaipWrTPCcsI7b+qv/80NRtXU3Dh2vv84u6DzXDhiqjJO4D791cFX62N798lPEbfXYXdgczB5rj7obxzgAZk8PBE/MyyINE5TZWnCIKu19RItx2gPWiD3Gli19uObrgg4+pLAMflMXIaJBEQQclPimuHQlESxBiIEoE8GiXhk2KNWZQma8FGMWecsOJClJRCUSLImBwl5YlRsvO757v/+joYJYKaoSiJuhVFyaoApZ5ACTgxX0840MRgOTGPekZUDl7dZr3lJBqkRTKBG81Eas2PukpsF6dp6v9eNw5S0/zMOCB9DdvINAHykNdykS0fWaiET+0+XT7aFsYR8jaOIwuXHHopr7jdQbvdA3FI/bTPWBQjkmtqoHtBMc65wu4GMaV9GGvxJgpIpQuqL6b0BjGKi65Qfc3KMU1hoCXAmSbNHqksC6ISD8xBHzi6B2iODolJwyYNyqlqAZ+J7VKMWkJNFIBMsENdBMnquZYVOy3SZLs9AGwpqXH7ZeOOs4JMlrl/PIBirxYCmPUB5g4SwYHQZ3ThyGf3+CI5OhcxGjJiGNrYw7VaIMniZoT2xcwGvqvpFIKcUUzLk0RyBfRtLzAd0LKh2zk/lyOAWFhAlqrSzjXCE6dwpnEu0HfU/H4AY5Wq5Bo9eSBRnKMW4hhWXrEH/mVPQJKCc1mK88jyOVokN9GhB5tjVBOH4b4+0obH0e83LnYMrztYeP3yS1bMyQM8pFpccbOqVqZDKrx8/PWFnSs3tjdvjl7cnH3zYAvT/Gw4O/79xvbzK3sXH53xznheZ+fVPSoRUntKHzCyjmMNbHhMPiLPyIcD2YcOVlgZSUCuq+IsC9coGcaZnKrLO5/dHz9+Jc+vJTgTOsLi6YQWHakqL2mKyhoGOpLxvUVfUjVmelaX2V35vC+STTzVIGiqEbb7aOvm6NK1nUvr4w3S4oXwoKJMLMxWRKGsYbvGWKCeGNhoCtfoWR1UCwmiYC3Mk1Cj58mwHd4VdWYxlPfzCNPn+68+e/3sOhpKrT8kcLefXdnd2hpvnIdnSwJ9sTG0FXvoS8/FhCL/Zx5HMJqCBqKXzmgCcc3+4U8k2R4jya9qkdErMJcgzKHdXPTlV5j2OHkIpExoQRXUHf/z7njjEjk7Q42JZQOsC/4eBitGIF0Usjzs2NEVv/A9dUQ+rFJ2T2+igekhOY0ujFJ81oT+YCxzTHV2Nqjxy7HRxUcc5Cqs8UlBcGpt+9k/SO91xuR3UlohH9pjb8vIB1fwJ8V+GkUnBvIhAd8wRWYYWurot7iwxBdSdVXKkc13NDqzYdcGKj9QqarKjj37thkR38S0GfFTHqobSdL1A8nmxZkOg+JBx12YacVQoQgWKWQN/5Mayin8VkpCLVDtqsJSm4ZvXamaFMGnRRfwshKcPgkSFWIl7qjoPCnkrMhFSZAPp88HDELLJG+B0OWPwJQbJjpMDFIqZy0jcGCjc59QExFRX2giiqEBYmTX0dveMAfSMgQufTU6jjEmfIxBC4xV8CcRkrSgwH5ie3NzdPku+UwAnv9GEaCqcsaEGZesWRgerKJ7nd3v/r773Qu/ILt+RUZLaEGmn0a4/WTv9mO4cBV/qiMZOAeWtCkDfVTA1Pug0ZCqVbnRQIm80ZCZenQKQdM7e1iY+g9q4usT')).decode('utf-8'))
+/**
+ *  https://ypapp.cnnb.com.cn/yongpai-user/api/duiba/autologin  链接下的userId
+ * 更新时间 1.16 自动获取抽奖ID
+ */
+const yongpai = "xxxxx#xxxxxx#xxxxxxx#xxxxxx"//格式为userId#deviceId支付宝账号#姓名 多账户&
+//手动点赞文章后https://ypapp.cnnb.com.cn/yongpai-ugc/api/praise/save_news?deviceId=的值 就是deviceId
+//deviceId 一般是16位  必须真机抓 瞎写的的点赞不成功
+const axios = require("axios").default;
+var lodash = require('lodash');
+const $ = new Env("甬派");
+const Notify = 0;
+let envSplitor = "&"
+window = {}
+function getUuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
+async function get_id() {
+    let url = `https://ypapp.cnnb.com.cn/yongpai-news/api/v2/news/list?channelId=0&currentPage=1&timestamp=${new Date().getTime()}`;
+    let res = await $.get(url);
+    for (let news of res.data.content) {
+        if (news.keywords) {
+            if (news.keywords.indexOf("转盘") !== -1) {
+                //console.log(news.channel[0].newsId)
+                let newsId = news.channel[0].newsId
+                url = `https://ypapp.cnnb.com.cn/yongpai-news/api/news/detail?newsId=${newsId}&userId=${$.userId}`;
+                res = await $.get(url);
+                $.id = res.data.body.split("?")[1].split("&")[0].replace("id=","")
+                //console.log( res.data.body.split("?")[1].split("&")[0].replace("id=",""))
+                return
+            }
+        }
+    }
+}
+async function get_autologin() {
+    let url = `https://ypapp.cnnb.com.cn/yongpai-user/api/duiba/autologin?dbredirect=https%3A//92722.activity-12.m.duiba.com.cn/hdtool/index?id%3D${$.id}%26dbnewopen&userId=${$.userId}`;
+    $.autologin = (await $.get(url)).data;
+}
+async function get_token() {
+    try {
+        await axios.get($.autologin, { "maxRedirects": 0 })
+    } catch (error) {
+        $.wdata = error.response.headers["set-cookie"].filter(c => c.includes("wdata")).join(";")
+    }
+    let url = `https://92722.activity-12.m.duiba.com.cn/hdtool/index?id=${$.id}&dbnewopen&from=login&spm=92722.1.1.1`;
+    let headers = {
+        'authority': '92722.activity-12.m.duiba.com.cn',
+        'accept': 'application/json',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'cookie': $.wdata,
+        'origin': 'https://92722.activity-12.m.duiba.com.cn',
+        'pragma': 'no-cache',
+        'referer': `https://92722.activity-12.m.duiba.com.cn/hdtool/index?id=${$.id}&dbnewopen&from=login&spm=92722.1.1.1`,
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    let res = await $.get(url, headers);
+    let code = /<script\b[^>]*>\s*var([\s\S]*?)<\/script>/.exec(res)[1];
+    //console.log(code)
+    eval(code)
+    global.key = /var\s+key\s+=\s+'([^']+)';/.exec(getDuibaToken.toString())[1];
+    //console.log(key)
+    url = 'https://92722.activity-12.m.duiba.com.cn/hdtool/ctoken/getTokenNew';
+    let data = `timestamp=${new Date().getTime()}&activityId=${$.id}&activityType=hdtool&consumerId=4066466507`;
+    res = eval((await $.post(url, data, headers)).token);
+    //console.log(window[key]);
+}
+async function doTask() {
+    let device_id = getUuid().toUpperCase()
+    let url = `https://ypapp.cnnb.com.cn/yongpai-news/api/v2/news/list?channelId=0&currentPage=1&timestamp=${new Date().getTime()}`;
+    let res = await $.get(url);
+    for (const item of res.data.content) {
+        let newsId = item.newsId;
+        url = `https://ypapp.cnnb.com.cn/yongpai-news/api/news/detail?newsId=${newsId}&userId=${$.userId}`;
+        res = await $.get(url);
+        res.message == 'OK' ? $.addMsg(`阅读：[${item.title}]:✅`) : $.addMsg(`阅读：[${item.title}]:❌`)
+        await $.wait(2);
+        let likeHeaders = {
+            "appversion": `10.1.4`,
+        }
+        url = `https://ypapp.cnnb.com.cn/yongpai-ugc/api/praise/save_news?deviceId=${$.deviceId}&newsId=${newsId}&userId=${$.userId}`
+        res = await $.get(url, likeHeaders);
+        //console.log(res)
+        res.message == 'OK' ? $.addMsg(`点赞：[${item.title}]:✅`) : $.addMsg(`点赞：[${item.title}]:❌`)
+        await $.wait(2);
+        url = `https://ypapp.cnnb.com.cn/yongpai-ugc/api/forward/news?newsId=${newsId}&source=4&userId=${$.userId}`
+        res = await $.get(url);
+        res.message == 'OK' ? $.addMsg(`分享：[${item.title}]:✅`) : $.addMsg(`分享：[${item.title}]:❌`)
+        await $.wait(2);
+    }
+    debugger
+}
+async function doJoin() {
+    let url = `https://92722.activity-12.m.duiba.com.cn/hdtool/doJoin?dpm=92722.3.1.0&activityId=${$.id}&_=${new Date().getTime()}`;
+    let data = `actId=${$.id}&oaId=${$.id}&activityType=hdtool&consumerId=4066466507&token=${window[key]}`
+    let headers = {
+        'authority': '92722.activity-12.m.duiba.com.cn',
+        'accept': 'application/json',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'cookie': $.wdata,
+        'origin': 'https://92722.activity-12.m.duiba.com.cn',
+        'pragma': 'no-cache',
+        'referer': `https://92722.activity-12.m.duiba.com.cn/hdtool/index?id=${$.id}&dbnewopen&from=login&spm=92722.1.1.1`,
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    let res = await $.post(url, data, headers);
+    $.addMsg(JSON.stringify(res))
+}
+async function doTakePrize() {
+    let url = `https://92722.activity-12.m.duiba.com.cn/crecord/getrecord?page=1&_=${new Date().getTime()}`;
+    let headers = {
+        'Accept': 'application/json',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        "Content-Type": "application/x-www-form-urlencoded",
+        //'Content-Length': '0',
+        'Cookie': $.wdata,
+        'Origin': 'https://92722.activity-12.m.duiba.com.cn',
+        'Pragma': 'no-cache',
+        'Referer': 'https://92722.activity-12.m.duiba.com.cn/activity/takePrizeNew?recordId=7074365348&dpm=92722.26.0.1&dcm=101.53.217692856808979.0&dbnewopen',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"'
+    }
+    let res = await $.get(url, headers);
+    let recordIds = res.records.filter(c => !c.quantity).map(c => (JSON.parse(c.emdJson).info));
+    for (const recordId of recordIds) {
+        url = `https://92722.activity-12.m.duiba.com.cn/activity/takePrizeNew?recordId=${recordId}&dpm=92722.26.0.1&dcm=101.53.217692856808979.0&dbnewopen`;
+        res = await $.get(url, headers);
+        let code = /<script\b[^>]*>\s*var([\s\S]*?)<\/script>/.exec(res)[1];
+        eval(code)
+        global.key = /var\s+key\s+=\s+'([^']+)';/.exec(getDuibaToken.toString())[1];
+        url = 'https://92722.activity-12.m.duiba.com.cn/ctoken/getToken.do';
+        res = eval((await $.post(url, '', headers)).token);
+        url = 'https://92722.activity-12.m.duiba.com.cn/activity/doTakePrize';
+        let data = `alipay=${encodeURIComponent($.account)}&realname=${encodeURIComponent($.realName)}&recordId=${recordId}&token=${window[key]}`
+        res = await $.post(url, data, headers)
+        console.log(JSON.stringify(res));
+    }
+}
+(async () => {
+    let arr = $.checkEnv("yp");
+    if (!arr) return await $.SendMsg("未填写token"), $.done();;
+    $._ = lodash.range(0, arr.length);
+    for (let index = 0; index < arr.length; index++) {
+        $.addMsg(`账号${index + 1}:`);
+        console.log(arr)
+        $.userId = arr[index].split("#")[0];
+        $.deviceId = arr[index].split("#")[1]
+        $.account = arr[index].split("#")[2];
+        $.realName = arr[index].split("#")[3];
+        console.log($.userId, $.account)
+        await doTask();
+        await get_id()
+        //$.id = 251912012489131
+        await get_autologin();
+        await get_token()
+        await doJoin()
+        await $.wait(10)
+        await doTakePrize()
+    }
+    await $.SendMsg($._msg);
+    $.done();
+})();
+function Env(name) {
+    return new (class {
+        constructor(name) {
+            this.name = name;
+            console.log(`\ud83d\udd14${this.name},\u5f00\u59cb!`);
+            Object.defineProperty(this, "random", {
+                get() {
+                    let i = lodash.random(0, this._.length - 1);
+                    let n = this._[i];
+                    this._.splice(i, 1);
+                    return n;
+                }
+            })
+        }
+        async get(url, headers) {
+            try {
+                return (await axios.get(url, { headers })).data;
+            } catch (err) {
+                console.log(`error:${err.message}`);
+                return this.get(url, headers);
+            }
+        }
+        async post(url, data, headers) {
+            try {
+                return (await axios.post(url, data, { headers: headers })).data;
+            } catch (err) {
+                console.log(`error:${err.message}`);
+                return this.post(url, data, headers);
+            }
+        }
+        async SendMsg(message) {
+            if (!message) return;
+            if (Notify > 0) {
+                var notify = require("./sendNotify");
+                await notify.sendNotify(this.name, message);
+            }
+        }
+        addMsg(msg) {
+            if (!this._msg) this._msg = "";
+            console.log(msg);
+            this._msg += msg + "\n";
+        }
+        wait(delay) {
+            return new Promise((res) => {
+                setTimeout(res, delay * 1000);
+            });
+        }
+        checkEnv() {
+            //let userCookie = ($.isNode() ? process.env[ckName] : $.getdata(ckName)) || "";
+            let userCookie = yongpai;
+            let userList = []
+            if (userCookie) {
+                let e = envSplitor[0];
+                for (let o of envSplitor)
+                    if (userCookie.indexOf(o) > -1) {
+                        e = o;
+                        break;
+                    }
+                for (let n of userCookie.split(e)) userList.push(n);
+                return userList
+            } else {
+                console.log("未找到CK");
+                return;
+            }
+        }
+        done() {
+            console.log(`\ud83d\udd14${this.name},\u7ed3\u675f!`);
+        }
+    })(name);
+}
