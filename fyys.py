2fd0a6e5fef1b8550f6ddce01d49b84dcef43269
yejigao <106310264+yejigao@users.noreply.github.com>
2024-05-26 22:19:06 +0800

    Add files via upload

--- fyys.py
+++ fyys.py
@@ -1,12 +1,11 @@
 """
-# new Env("七猫免费小说")
-# cron: 1 9,16-22 * * *
-
+# new Env("枫以影视")
+# cron: 1 8-20/2 * * *
 通用代码
 只需要修改  asyncio.run(common.main("qimao"))  "qimao"代表七猫脚本 "ydcd"代表 有道词典，现有hkread ,speedread,fyys,shengdu,kgyy,qimao
 代码请勿用于非法盈利，一切与本人无关，该代码仅用于学习交流，请阅览下载24小时内删除代码
 """
-SCRIPT_NAME = "qimao"   # 脚本名称 记得修改这里
+SCRIPT_NAME = "fyys"   # 脚本名称 记得修改这里
 
 
 import asyncio
--- 中青快应用.py
+++ 中青快应用.py
@@ -1,12 +1,23 @@
-"""
-# new Env("七猫免费小说")
-# cron: 1 9,16-22 * * *
+# 中青快应用 邀请code https://user.youth.cn/h5/fastAppWeb/invite/invite_ground_2.html?share_uid=1038191728&channel=c8000&nickname=%E5%91%A8%E6%A0%91%E4%BA%BA%E9%B2%81%E8%BF%85&avatar=http%3A%2F%2Fres.youth.cn%2Favatar_202203_28_28n_6241677754c3f1038191728m.jpg&v=1697879197
+# author @wangquanfugui233
+# cron 一天一次 一次3毛，自动提现 微信支付宝都可以
+# python3.10.X 64位可以运行，别的没适配
+# 抓userinfo? 只要问号后面的参数就行 
+# 变量    自动提现： 1是微信 0是支付宝，不填不行,多账户就@分开 
+# export zqurl='url?后面的参数#1是微信#true（是当够10元就提10不填就不提，打开可能导致黑号）'
+# export zqurl='zzzzzz#1@cccccccccc#0'
+# 如果想提高一点收益可以 export zq_open='true' 可不填
+# github上的so为旧版
+# new Env("中青快应用")
+# cron: 1 12 * * *
 
+
+"""
 通用代码
 只需要修改  asyncio.run(common.main("qimao"))  "qimao"代表七猫脚本 "ydcd"代表 有道词典，现有hkread ,speedread,fyys,shengdu,kgyy,qimao
 代码请勿用于非法盈利，一切与本人无关，该代码仅用于学习交流，请阅览下载24小时内删除代码
 """
-SCRIPT_NAME = "qimao"   # 脚本名称 记得修改这里
+SCRIPT_NAME = "zqfast"   # 脚本名称 记得修改这里
 
 
 import asyncio
